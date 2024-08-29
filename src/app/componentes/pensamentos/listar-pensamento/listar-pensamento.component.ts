7910afbd965c40a7b7ccf4301a45a27efcf85bb5
Leandro Gaspar <lsg.gaspar@gmail.com>
2024-08-29 06:29:34 -0300

    Uso de diretivas

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent {
-  listaPensamento = [];
+  listaPensamento = [
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Leandro',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'IIS - Rodam as aplicações GSUB (crítica), "portalocn" (não há código de aplicação, apesar de mostrar informações do banco do SQDO) e A17367 ("gissub-pdp"), ASP Clássico, https://gissub.petrobras.biz, https://gissub-pdp.petrobras.biz, https://gissub-car.petrobras.biz e https://gissub-noaa.petrobras.biz;',
+      autoria: 'Monica',
+      modelo: 'modelo2'
+    }
+  ];
 }
